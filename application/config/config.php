c811d49708515fd886ea9534acb2970e3995bfd7
alex <smartmanage715@gmail.com>
2022-11-04 06:35:58 +0900

    set config

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$root = "http://".$_SERVER['HTTP_HOST'];
+$root = "https://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url']    = $root;
 
@@ -536,7 +536,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-$config['installed'] = TRUE;
+$config['installed'] = FALSE;
 
 
 $config['admin_per_page'] = 20;
