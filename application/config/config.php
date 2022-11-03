08f7bbeb52b0657b5c07126fcd8b5aa5d8c1b239
alex <smartmanage715@gmail.com>
2022-11-04 06:30:50 +0900

    set db

--- application/config/config.php
+++ application/config/config.php
@@ -536,7 +536,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-$config['installed'] = FALSE;
+$config['installed'] = TRUE;
 
 
 $config['admin_per_page'] = 20;
